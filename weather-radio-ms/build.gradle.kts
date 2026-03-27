a7d0d13643cc3997991201324598d790664116c6
Jon Brule <brulejr@tgmail.com>
2026-03-27 19:29:34 -0400

    Integrates local KSB Commons dependency

--- weather-radio-ms/build.gradle.kts
+++ weather-radio-ms/build.gradle.kts
@@ -1,3 +1,11 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+val ksbCommonsVersion: String by project
+val projectVersion: String by project
+
+val useLocalKsbCommonsString: String = "true"
+val useLocalKsbCommons: Boolean = useLocalKsbCommonsString.toBoolean()
+
 plugins {
     kotlin("jvm") version "2.2.10"
     kotlin("plugin.spring") version "2.2.10"
@@ -6,8 +14,6 @@ plugins {
 }
 
 group = "io.jrb.labs"
-version = "0.0.1-SNAPSHOT"
-
 description = "Offline NOAA weather radio phase 1 stub service"
 
 java {
@@ -16,13 +22,54 @@ java {
     }
 }
 
+/**
+ * Resolve the project version:
+ *
+ * - In CI on a tag build, GITHUB_REF_NAME will be something like "v0.3.1".
+ *   We strip the leading "v" and use "0.3.1" as the version.
+ * - Otherwise, fall back to projectVersion from gradle.properties.
+ */
+version = System.getenv("GITHUB_REF_NAME")
+    ?.let { refName ->
+        if (refName.matches(Regex("""v\d+\.\d+\.\d+"""))) {
+            refName.removePrefix("v")
+        } else {
+            projectVersion
+        }
+    }
+    ?: projectVersion
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
 repositories {
     mavenCentral()
+
+    if (!useLocalKsbCommons) {
+        maven {
+            url = uri("https://maven.pkg.github.com/brulejr/ksb-commons")
+            credentials {
+                username = findProperty("gpr.user") as String?
+                    ?: System.getenv("GITHUB_PACKAGES_USER")
+                            ?: System.getenv("GITHUB_ACTOR")
+                            ?: "brulejr"
+                password = findProperty("gpr.key") as String?
+                    ?: System.getenv("GITHUB_PACKAGES_TOKEN")
+                            ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 dependencies {
-    implementation(platform("io.jrb.labs:ksb-commons-bom:$ksbCommonsVersion"))
-    implementation("io.jrb.labs:ksb-core")
+    if (useLocalKsbCommons) {
+        implementation(platform("io.jrb.labs:ksb-dependency-bom"))
+    } else {
+        implementation(platform("io.jrb.labs:ksb-dependency-bom:$ksbCommonsVersion"))
+    }
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -39,5 +86,9 @@ tasks.withType<Test> {
 }
 
 kotlin {
-    jvmToolchain(21)
+    compilerOptions {
+        freeCompilerArgs.add("-Xjsr305=strict")
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
 }
+
